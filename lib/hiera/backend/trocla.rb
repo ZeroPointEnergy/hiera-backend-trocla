005fc0cdf049e24cd69a59f0ffe1b79d2af5a199
Andreas Zuber <zuber@puzzle.ch>
2015-11-06 15:50:37 +0100

    gem creation with bundler

---
+++ lib/hiera/backend/trocla.rb
@@ -0,0 +1,9 @@
+require "hiera/backend/trocla/version"
+
+module Hiera
+  module Backend
+    module Trocla
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/hiera/backend/trocla/version.rb
@@ -0,0 +1,7 @@
+module Hiera
+  module Backend
+    module Trocla
+      VERSION = "0.0.1"
+    end
+  end
+end
